d0c0a3524e6ca3f5c15abbab500bd9a66116aa6e
Jonathan on Swift <jonathan.lassen@gmail.com>
2019-06-25 14:49:27 -0400

    db permissions stuff

--- src/config.js
+++ src/config.js
@@ -1,9 +1,12 @@
 'use strict';
 module.exports = {
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://spaced-repetition-client-david-jonathan.jonathanlassen.now.sh',
+  //CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://spaced-repetition-client-david-jonathan.jonathanlassen.now.sh',
+   CLIENT_ORIGIN:'http://localhost:3000',
+
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DATABASE_URL || 'postgresql://New@localhost/spaced-repetition',
+ // DB_URL: process.env.DATABASE_URL || 'postgresql://New@localhost/spaced-repetition',
+  DB_URL: process.env.DATABASE_URL || 'postgresql://dunder-mifflin@localhost/spaced-repetition',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 };
