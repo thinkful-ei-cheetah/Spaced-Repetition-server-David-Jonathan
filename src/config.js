40719f569dbb774f8c764b67b22fdbb864c624af
David Nordeen <davidnordeen@gmail.com>
2019-06-25 07:59:20 -0700

    updated config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = {
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://spaced-repetition-client-david-jonathan.jonathanlassen.now.sh',
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DATABASE_URL || 'postgresql://New@localhost/spaced-repetition',
