bec702406137ea2c8b0d5f3b1683f9531aa25b25
David Nordeen <davidnordeen@gmail.com>
2019-06-28 09:11:28 -0700

    final deploy

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
-  //CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://spaced-repetition-client-david-jonathan.jonathanlassen.now.sh',
-  CLIENT_ORIGIN: 'http://localhost:3000',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://spaced-repetition-client-david-jonathan.jonathanlassen.now.sh',
+  // CLIENT_ORIGIN: 'http://localhost:3000',
 
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
